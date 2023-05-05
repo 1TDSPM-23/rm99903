f700abca3c3537e736c77ff364808db3ce7999c3
Allan-Perca <a.percario@hotmail.com>
2023-05-05 09:55:24 -0300

    Array push/unshift/indexOf/splice/forEach

--- js/script.js
+++ js/script.js
@@ -51,18 +51,18 @@ console.log("Olá Mundo!");
     // console.log(`O NÚMERO É: ${resultado}`);
 
 //diferença entre var/let/const -- var e let fazem basicamente a msm coisa mas comportamentos diferentes
-    var nome = "Beatriz"; 
-    if (nome != ""){
-        var nome = "Robson";
-    }
-    console.log(nome) // vai imprimir o robson msm que está fechado em bloco, vai trocar o valor da variavel msm fora do if
+//    var nome = "Beatriz"; 
+//    if (nome != ""){
+//        var nome = "Robson";
+//    }
+//    console.log(nome) // vai imprimir o robson msm que está fechado em bloco, vai trocar o valor da variavel msm fora do if
 
-    let nome2 = "Matheus";
-    if (nome != ""){
-        let nome2 = "Bia"
-        console.log("Nome do if: ", nome2)
-    }    
-    console.log(nome2)//aqui irá respeitar o nome matheus, o nome bia está dentro do laço ainda
+//    let nome2 = "Matheus";
+//    if (nome != ""){
+//        let nome2 = "Bia"
+//        console.log("Nome do if: ", nome2)
+//    }    
+//    console.log(nome2)//aqui irá respeitar o nome matheus, o nome bia está dentro do laço ainda
 
   //Regras do const
   // 1 - inicializar na declaração    
@@ -71,12 +71,36 @@ console.log("Olá Mundo!");
 //    nome3 = "John" - nao pode fazer a mudança
 //    const PI = 3.14; // pi n muda, sempre será o msm
 
+//ARRAY E SEUS METODOS
 
+let frutas = ["banana", "maça", "morango", "carambola", "acerola", "laranja"];
 
+//imprimindo um array
+console.log(frutas);
+console.table(frutas);
 
+//adicionar item no final do array
+frutas.push("melão");
+console.log(frutas);
 
+//adicionar item no inicio do array
+frutas.unshift("caju");
+console.log(frutas);
 
+//para localizar um item dentro do array utilizamos o metodo indexOf(item_nome)
+//let indice = frutas.indexOf("morango");
+//console.log(frutas[indice]);
 
+//para localizar um item do array e apagar ele utilizamos o metodo splice(indice do item, quantidade de vezes que o indice será removido)
+//obs: utilize o metodo indexOf(item_nome) para obter o indice do item
+let indice = frutas.indexOf("carambola");
+frutas.splice(indice, 1); // se n declarar o numero de vezes ele apaga a posição ate acabar a lista
+console.log(frutas);
+
+// ForEach - estrutura de repetição interna do proprio array
+frutas.forEach( (fruta)=>{
+    console.log("Olha ele: " + fruta)
+}) 
 
 
 
