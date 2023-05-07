47016f4f93d172f807c0345ae850b95b1054f7d7
Allan-Perca <a.percario@hotmail.com>
2023-05-07 19:42:41 -0300

    exc1 js html

--- js/exercicio-1.js
+++ js/exercicio-1.js
@@ -3,13 +3,22 @@ function mudaDeCor(){
 }
 
 
-function formulario(){
-    let list = [];
-    constnewItem = document.createElement('li');
-    list.appendChild(newItem);
+function adicionarItem(){
+    const texto = document.getElementById('novoItem').value;
+    const newItem = document.createElement('li');
+    newItem.textContent = texto;
+    document.getElementById('lista').appendChild(newItem);
+}
+
+function mudaImagem(){
+    document.getElementById('imagem').src='https://img.freepik.com/vetores-premium/desenho-de-fantasma-de-halloween_71328-356.jpg?w=2000'
+}
+
+function mensagem() {
+    document.getElementById('mostrarMensagem').innerText='mensagem';
 }
 
 
-
-
-
+function ocultaImagem() {
+    document.getElementById('ocultar').hidden= true;
+}
